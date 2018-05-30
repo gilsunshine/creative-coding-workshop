cc8f4aad04274839b86eb0ec20ea9888d386f58b
gilsunshine <gilsunshine@gmail.com>
2018-05-30 15:16:26 -0400

    file setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# creative-coding-workshop
+#Creative Coding with p5
+
+Creative coding is the process of using computer programming as a tool for art and design. 
---
+++ sketch.js
@@ -0,0 +1,10 @@
+//runs once
+function setup(){
+  //specify a width and height for our drawing area
+  createCanvas(windowWidth, windowHeight)
+}
+
+//runs on a loop
+function draw(){
+  ellipse(windowWidth/2, windowHeight/2, 50)
+}
